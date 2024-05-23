830cc037c2b95941c85965920e12926efea73e18
tux <gustavo.torres.d@tecsup.edu.pe>
2024-05-22 20:37:55 -0500

    workflow for dev test finished6

--- .github/scripts/sync.sh
+++ .github/scripts/sync.sh
@@ -6,13 +6,13 @@ BRANCH="${GITHUB_REF#refs/heads/}"
 if [ "$BRANCH" = "master" ]; then
   status=$(curl -s -w "%{http_code}" -X PATCH $DATABRICKS_WORKSPACE/api/2.0/repos/prodxd -H "Authorization: Bearer $DATABRICKS_TOKEN" -H "Content-Type: application/json" -d '{"branch": "master"}')
 else
-  # status=$(curl -s -w "%{http_code}" -X PATCH $DATABRICKS_WORKSPACE_DEV/api/2.0/repos/751319337702225 -H "Authorization: Bearer $DATABRICKS_TOKEN_DEV" -H "Content-Type: application/json" -d '{"branch": "dev"}')
-  status=$(curl -s -w "%{http_code}" -X PATCH $DATABRICKS_WORKSPACE_DEV/api/2.0/repos/3991601147850106 -H "Authorization: Bearer $DATABRICKS_TOKEN_DEV" -H "Content-Type: application/json" -d '{"branch": "dev"}')
+  status=$(curl -s -w "%{http_code}" -X PATCH $DATABRICKS_WORKSPACE_DEV/api/2.0/repos/751319337702225 -H "Authorization: Bearer $DATABRICKS_TOKEN_DEV" -H "Content-Type: application/json" -d '{"branch": "dev"}')
+  # status=$(curl -s -w "%{http_code}" -X PATCH $DATABRICKS_WORKSPACE_DEV/api/2.0/repos/3991601147850106 -H "Authorization: Bearer $DATABRICKS_TOKEN_DEV" -H "Content-Type: application/json" -d '{"branch": "dev"}')
 fi
 
 status_code=$(echo "$status" | grep -oE '[0-9]{3}$')
 
-if [ $status_code == "200" ]; then
+if [ $status_code = "200" ]; then
     echo "Job Succesful response was "$status_code
 else
     echo "Job Failed Response was "$status_code
