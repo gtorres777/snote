b34a6fb8a00d75a281697fada10f147aae631e34
tux <gustavo.torres.d@tecsup.edu.pe>
2024-05-22 19:26:37 -0500

    workflow for dev test finished2

--- .github/scripts/sync.sh
+++ .github/scripts/sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $GITHUB_BRANCH == "master" ]]; then
+if [ $GITHUB_BRANCH == "master" ]; then
   status=$(curl -s -w "%{http_code}" -X PATCH $DATABRICKS_WORKSPACE/api/2.0/repos/prodxd -H "Authorization: Bearer $DATABRICKS_TOKEN" -H "Content-Type: application/json" -d '{"branch": "master"}')
 else
   status=$(curl -s -w "%{http_code}" -X PATCH $DATABRICKS_WORKSPACE_DEV/api/2.0/repos/751319337702225 -H "Authorization: Bearer $DATABRICKS_TOKEN_DEV" -H "Content-Type: application/json" -d '{"branch": "master"}')
@@ -8,7 +8,7 @@ fi
 
 status_code=$(echo "$status" | grep -oE '[0-9]{3}$')
 
-if [[ $status_code == "200" ]]; then
+if [ $status_code == "200" ]; then
     echo "Job Succesful response was "$status_code
 else
     echo "Job Failed Response was "$status_code
