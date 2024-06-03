025f3e46dc79a7feefc2755b29af95a101cb5cb8
tux <gustavo.torres.d@tecsup.edu.pe>
2024-06-02 23:18:08 -0500

    test envs9f

--- test/getemail.py
+++ test/getemail.py
@@ -1,2 +1,8 @@
 # Databricks notebook source
+import os
+
+s3_path = os.getenv('S3_PATH', 's3://default-path')
+print("S3PATH")
+print(s3_path)
+
 print("email sent porfin xD dev7 xDDD")
--- test/gethi.py
+++ test/gethi.py
@@ -1,2 +1,8 @@
 # Databricks notebook source
+import os
+
+db_url = os.getenv('DB_URL', 'defaultMYSQL')
+print("DBURL")
+print(db_url)
+
 print("hola mundo")
